b16f8e7cedcf34d16876cb34790a17b1f02a95dc
RQAJohnson <RQAJohnson@qa.com>
2021-02-08 13:05:40 +0000

    Domain features added for repo

--- src/main/java/com/qa/persistance/repo/SchoolRepo.java
+++ src/main/java/com/qa/persistance/repo/SchoolRepo.java
@@ -2,6 +2,8 @@ package com.qa.persistance.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.qa.persistance.domain.SchoolDomain;
+
 public interface SchoolRepo extends JpaRepository<SchoolDomain, Long> {
 
 }
--- src/main/java/com/qa/persistance/repo/TeacherRepo.java
+++ src/main/java/com/qa/persistance/repo/TeacherRepo.java
@@ -2,6 +2,8 @@ package com.qa.persistance.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.qa.persistance.domain.TeacherDomain;
+
 public interface TeacherRepo extends JpaRepository<TeacherDomain, Long> {
 
 }
