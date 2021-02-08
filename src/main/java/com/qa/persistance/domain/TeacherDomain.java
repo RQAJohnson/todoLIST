0904c4183ea0b0529ee522c90eb76ea64f19323f
RQAJohnson <RQAJohnson@qa.com>
2021-02-08 07:27:35 +0000

    Teacher Domain added

--- src/main/java/com/qa/persistance/domain/TeacherDomain.java
+++ src/main/java/com/qa/persistance/domain/TeacherDomain.java
@@ -1,5 +1,15 @@
 package com.qa.persistance.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class TeacherDomain {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
+	private Long id;
 
 }
