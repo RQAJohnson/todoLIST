506d146a50f6af0957d01922929b53a79975b883
RQAJohnson <RQAJohnson@qa.com>
2021-02-08 07:16:19 +0000

    Teacher DTO finished

--- src/main/java/com/qa/persistance/dto/TeacherDTO.java
+++ src/main/java/com/qa/persistance/dto/TeacherDTO.java
@@ -2,6 +2,55 @@ package com.qa.persistance.dto;
 
 public class TeacherDTO {
 	private Long id;
+	
+	private String name;
+	private String subject;
+	
+	
+	public TeacherDTO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public TeacherDTO(Long id, String name, String subject) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.subject = subject;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getSubject() {
+		return subject;
+	}
+
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	
+	
 
 	
 }
