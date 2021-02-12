53c72d4ac7d026ae7cfa131b4e1f05af135ed766
RQAJohnson <RQAJohnson@qa.com>
2021-02-12 15:32:09 +0000

    changes to integration testing

--- src/test/java/com/qa/rest/TeacherControllerTEST.java
+++ src/test/java/com/qa/rest/TeacherControllerTEST.java
@@ -1,5 +1,16 @@
 package com.qa.rest;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@Sql(scripts = {"classpath:data-test.sql"}, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+@ActiveProfiles(profiles="test")
 public class TeacherControllerTEST {
 
 }
