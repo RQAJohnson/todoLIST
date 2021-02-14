efe685953392e8c5383c40618e7a3055f93a8ad2
RQAJohnson <RQAJohnson@qa.com>
2021-02-14 22:57:01 +0000

    Try to improve coverage

--- src/test/java/com/qa/rest/SchoolControllerTEST.java
+++ src/test/java/com/qa/rest/SchoolControllerTEST.java
@@ -1,25 +1,127 @@
 package com.qa.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import com.qa.service.SchoolService;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.qa.persistance.domain.SchoolDomain;
+import com.qa.persistance.dto.SchoolDTO;
+
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@Sql(scripts = {"classpath:schema-test.sql","classpath:data-test.sql"}, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+@ActiveProfiles(profiles="test")
 public class SchoolControllerTEST {
 	
-	@RestController
-	@RequestMapping("/school")
-	public class SchoolController {
+	@Autowired
+	private MockMvc mock;
+	
+	@Autowired
+	private ModelMapper mapper;
+	
+	@Autowired
+	private ObjectMapper jsonifier; 
+	
+	private final int ID = 1;
+	
+	private SchoolDTO mapToDTO(SchoolDomain model) {
+		return this.mapper.map(model, SchoolDTO.class);
+	}
+	
+	@Test
+	public void create() throws Exception {
 		
-		private SchoolService service;
-
-		public SchoolController(SchoolService service) {
-			super();
-			this.service = service;
+		// List<TeacherDomain> teacherList
+		SchoolDomain contentBody = new SchoolDomain(1L, "25 Langley Lane", null);
+		SchoolDTO expectedResult = mapToDTO(contentBody);
+		 
+		//set up request
+		MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders
+				.request(HttpMethod.POST, "/school/create/")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(jsonifier.writeValueAsString(contentBody))
+				.accept(MediaType.APPLICATION_JSON);
+		
+		//isOk. If integer .204 OR HttpStatus.ACCEPTED **set up expected status**
+		ResultMatcher matchStatus = MockMvcResultMatchers.status().isCreated();  		
+		ResultMatcher matchContent = MockMvcResultMatchers.content().json(jsonifier.writeValueAsString(expectedResult));
+		
+		//PERFORM
+		this.mock.perform(mockRequest).andExpect(matchStatus);
+		this.mock.perform(mockRequest).andExpect(matchContent);
+		
 		}
-			
-			
-//		TEST	
+	
+	
+//	READ
+	@Test
+	public void readAll() throws Exception {
+//		TeacherDomain contentBody = new TeacherDomain(3L,"Barry","Science", null);
+//		TeacherDTO expectedResult = mapToDTO(contentBody);
+//		
+//		//set up request
+//		MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders
+//				.request(HttpMethod.GET, "/teacher/readAll/")
+//				.contentType(MediaType.APPLICATION_JSON)
+//				.content(jsonifier.writeValueAsString(contentBody))
+//				.accept(MediaType.APPLICATION_JSON);
+//		
+//		//isOk. If integer .204 OR HttpStatus.ACCEPTED **set up expected status**
+//		ResultMatcher matchStatus = MockMvcResultMatchers.status().isOk();  		
+//		ResultMatcher matchContent = MockMvcResultMatchers.content().json(jsonifier.writeValueAsString(expectedResult));
+//		
+//		//PERFORM
+//		this.mock.perform(mockRequest).andExpect(matchStatus);
+//		this.mock.perform(mockRequest).andExpect(matchContent);
 
 	}
-}
+	
+//	READ BY ID
+	@Test
+	public void readSchool() throws Exception {
+		SchoolDTO expectedResult = new SchoolDTO(1L, "25 Langley Lane", null);
+		
+		MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders
+				.request(HttpMethod.GET, "/school/read/" + ID); //set up request
+		
+		//isOk. If integer .204 OR HttpStatus.ACCEPTED **set up expected status**
+		ResultMatcher matchStatus = MockMvcResultMatchers.status().isOk();  		
+		
+		//PERFORM
+		this.mock.perform(mockRequest).andExpect(matchStatus);
+		
+	}
+	
+	
+//	UPDATE	
+	@Test
+	public void update() throws Exception{
+		
+	}
+	
+//	DELETE
+	@Test
+	public void delete() throws Exception {
+		
+			
+	}
+	
+		
+	}
+
--- src/test/java/com/qa/rest/TeacherControllerTEST.java
+++ src/test/java/com/qa/rest/TeacherControllerTEST.java
@@ -1,16 +1,159 @@
 package com.qa.rest;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.qa.persistance.domain.TeacherDomain;
+import com.qa.persistance.dto.TeacherDTO;
+import com.qa.service.TeacherService;
+
+
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-@Sql(scripts = {"classpath:data-test.sql"}, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = {"classpath:schema-test.sql","classpath:data-test.sql"}, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
 @ActiveProfiles(profiles="test")
 public class TeacherControllerTEST {
+	
+	private static final TeacherService TeacherService = null;
+
+	@Autowired
+	private MockMvc mock;
+	
+	@Autowired
+	private ModelMapper mapper;
+	
+	@Autowired
+	private ObjectMapper jsonifier;
+	
+	private final int ID = 1;
+	
+	private TeacherDTO mapToDTO(TeacherDomain model) {
+		return this.mapper.map(model, TeacherDTO.class);
+	}
+	
+
+//	POST or CREATE
+	@Test
+	public void create() throws Exception {
+		
+		TeacherDomain contentBody = new TeacherDomain(3L,"Barry","Science", null);
+		TeacherDTO expectedResult = mapToDTO(contentBody);
+		 
+		//set up request
+		MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders
+				.request(HttpMethod.POST, "/teacher/create/")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(jsonifier.writeValueAsString(contentBody))
+				.accept(MediaType.APPLICATION_JSON);
+		
+		//isOk. If integer .204 OR HttpStatus.ACCEPTED **set up expected status**
+		ResultMatcher matchStatus = MockMvcResultMatchers.status().isCreated();  		
+		ResultMatcher matchContent = MockMvcResultMatchers.content().json(jsonifier.writeValueAsString(expectedResult));
+		
+		//PERFORM
+		this.mock.perform(mockRequest).andExpect(matchStatus);
+		this.mock.perform(mockRequest).andExpect(matchContent);
+		
+		}
+	
+	
+//	READ
+	@Test
+	public void readAll() throws Exception {
+//		TeacherDomain contentBody = new TeacherDomain(3L,"Barry","Science", null);
+//		TeacherDTO expectedResult = mapToDTO(contentBody);
+//		
+//		MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders
+//				.request(HttpMethod.GET, "/teacher/readAll/"); //set up request
+//		
+//		
+//		TeacherController teacherContoller = new TeacherController(TeacherService);
+//		List<TeacherDTO> teachers = new ArrayList<>();
+//		teachers.add(new TeacherDTO(3L,"Chris", "P.E"));
+		
+//		Mockito.when(contentBody.readAll()).thenReturn(teachers);
+//		assertEquals(teachers, TeacherController.readAll());
+//		
+//		customers.add(new Customer("Rhys", "T"));
+//		customers.add(new Customer("Nic", "J"));
+		
+		
+//		
+//		//set up request
+//		MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders
+//				.request(HttpMethod.GET, "/teacher/readAll/")
+//				.contentType(MediaType.APPLICATION_JSON)
+//				.content(jsonifier.writeValueAsString(contentBody))
+//				.accept(MediaType.APPLICATION_JSON);
+//		
+//		//isOk. If integer .204 OR HttpStatus.ACCEPTED **set up expected status**
+//		ResultMatcher matchStatus = MockMvcResultMatchers.status().isOk();  		
+//		ResultMatcher matchContent = MockMvcResultMatchers.content().json(jsonifier.writeValueAsString(expectedResult));
+//		
+//		//PERFORM
+//		this.mock.perform(mockRequest).andExpect(matchStatus);
+//		this.mock.perform(mockRequest).andExpect(matchContent);
+
+	}
+	
+//	READ BY ID
+	@Test
+	public void readTeacher() throws Exception {
+		TeacherDTO expectedResult = new TeacherDTO(1L, "sarah", "maths");
+		
+		MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders
+				.request(HttpMethod.GET, "/teacher/read/" + ID); //set up request
+		
+		//isOk. If integer .204 OR HttpStatus.ACCEPTED **set up expected status**
+		ResultMatcher matchStatus = MockMvcResultMatchers.status().isOk();  		
+		ResultMatcher matchContent = MockMvcResultMatchers.content().json(jsonifier.writeValueAsString(expectedResult));
+		
+		//PERFORM
+		this.mock.perform(mockRequest).andExpect(matchStatus);
+		this.mock.perform(mockRequest).andExpect(matchContent);
+		
+		
+	}
+	
+//	UPDATE	
+	@Test
+	public void update() throws Exception{
+		
+	}
+	
+//	DELETE
+	
+	
+	@Test
+	public void delete() throws Exception {
+//		Long id = 1L;
+//		Mockito.doReturn(id).when(TeacherService).delete(id);
+//		TeacherService.delete(id);
+//		Mockito.verify(TeacherService, Mockito.times(1)).delete(1L);
+		
+			
+	}
+	
 
 }
