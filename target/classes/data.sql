fb72dac62710763cc57bf83bea492a6e18df5e0a
RQAJohnson <RQAJohnson@qa.com>
2021-02-16 10:25:31 +0000

    target folder added

---
+++ target/classes/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO SCHOOL_DOMAIN(ADDRESS)
+VALUES
+('25 Langley Lane'),
+('612 Bishop Road'),
+('206 Charleston Highway'),
+('12 Northside Avenue');
+
+
+INSERT INTO TEACHER_DOMAIN(NAME, SUBJECT, MY_SCHOOL_ID)
+VALUES
+('sarah','maths',2),
+('harry','biology',1),
+('kate', 'P.E', 3);
